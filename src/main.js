b2c6d4f3b41d59903e0bafdf5dc5dab85ec7b267
Tolga Çağlayan <tolgaand@yandex.com>
2021-05-11 22:34:09 +0300

    Implement antdesign components

--- src/main.js
+++ src/main.js
@@ -3,7 +3,51 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-import 'ant-design-vue/dist/antd.css';
+import "ant-design-vue/dist/antd.css";
+
+import {
+  Button,
+  Input,
+  Icon,
+  Layout,
+  Row,
+  Col,
+  Comment,
+  Tooltip,
+  Avatar,
+  Card,
+  Tag,
+  Radio,
+  Menu,
+  Form,
+  Alert,
+  Empty,
+  Tabs,
+  Result,
+} from "ant-design-vue";
+
+const components = [
+  Button,
+  Input,
+  Icon,
+  Layout,
+  Row,
+  Col,
+  Comment,
+  Tooltip,
+  Avatar,
+  Card,
+  Tag,
+  Radio,
+  Menu,
+  Form,
+  Alert,
+  Empty,
+  Tabs,
+  Result,
+];
+
+components.forEach((c) => Vue.use(c));
 
 Vue.config.productionTip = false;
 
