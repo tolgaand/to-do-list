24fdc6a2594c70f706dddf6352086f16e693ae01
Tolga Çağlayan <tolgaand@yandex.com>
2021-05-11 22:33:36 +0300

    Change the router active class name and implement other pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
@@ -8,21 +7,18 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: () => import("../layouts/MainLayout.vue"),
+    children: [
+      { path: "", component: () => import("../views/Home.vue") },
+      { path: "/login", component: () => import("../views/Login.vue") },
+      { path: "/register", component: () => import("../views/Register.vue") },
+    ],
   },
 ];
 
 const router = new VueRouter({
   mode: "history",
+  linkActiveClass: "ant-menu-item-selected",
   base: process.env.BASE_URL,
   routes,
 });
