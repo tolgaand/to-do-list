36da4fb985640c83ca72a21965f2a2f59f88823d
Tolga Çağlayan <tolgaand@yandex.com>
2021-05-14 19:06:22 +0300

    Configure routes and pull a store object

--- src/router/index.js
+++ src/router/index.js
@@ -3,24 +3,44 @@ import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: () => import("../layouts/MainLayout.vue"),
-    children: [
-      { path: "", component: () => import("../views/Home.vue") },
-      { path: "/login", component: () => import("../views/Login.vue") },
-      { path: "/register", component: () => import("../views/Register.vue") },
+export default function init(store) {
+  return new VueRouter({
+    mode: "history",
+    linkActiveClass: "ant-menu-item-selected",
+    base: process.env.BASE_URL,
+    routes: [
+      {
+        path: "/",
+        name: "Home",
+        component: () => import("../layouts/MainLayout.vue"),
+        children: [
+          { path: "", component: () => import("../views/Home.vue") },
+          {
+            path: "/dashboard",
+            component: () => import("../views/Dashboard.vue"),
+            beforeEnter(to, from, next) {
+              if (!store.state.user) return next("/login");
+              next();
+            },
+          },
+          {
+            path: "/login",
+            component: () => import("../views/Login.vue"),
+            beforeEnter(to, from, next) {
+              if (store.state.user) return next("/");
+              next();
+            },
+          },
+          {
+            path: "/register",
+            component: () => import("../views/Register.vue"),
+            beforeEnter(to, from, next) {
+              if (store.state.user) return next("/");
+              next();
+            },
+          },
+        ],
+      },
     ],
-  },
-];
-
-const router = new VueRouter({
-  mode: "history",
-  linkActiveClass: "ant-menu-item-selected",
-  base: process.env.BASE_URL,
-  routes,
-});
-
-export default router;
+  });
+}
