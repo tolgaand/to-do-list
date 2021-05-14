b4a233f7db2d61036af009ff891c741e55566f7d
Tolga Çağlayan <tolgaand@yandex.com>
2021-05-14 18:58:08 +0300

    Remove all menu links except Home and move the script up

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,3 +1,14 @@
+<script>
+import { mapState } from "vuex";
+export default {
+  data: () => ({
+    collapsed: false,
+  }),
+  computed: {
+    ...mapState(["user"]),
+  },
+};
+</script>
 <template lang="pug">
   a-layout#components-layout-demo-custom-trigger
     a-layout-sider(v-model="collapsed" :trigger="null" collapsible)
@@ -8,46 +19,13 @@
           router-link(to="/" role="menu-item" style="padding-left:24px;" class="ant-menu-item" tag="li" exact)
             a-icon(type="home")
             span Home
-          router-link(to="/login" role="menu-item" style="padding-left:24px;" class="ant-menu-item" tag="li" exact)
-            a-icon(type="login")
-            span Login
-          router-link(to="/logout" role="menu-item" style="padding-left:24px;" class="ant-menu-item" tag="li" exact)
-            a-icon(type="logout")
-            span Logout
-          router-link(to="/register" role="menu-item" style="padding-left:24px;" class="ant-menu-item" tag="li" exact)
-            a-icon(type="smile")
-            span Aramıza Katıl
-          //- a-menu-item(key="2")
-          //-   router-link(to="/login" exact)
-          //-     a-icon(type="login")
-          //-     span Login
-          //- a-menu-item(key="3")
-          //-   router-link(to="/register")
-          //-     a-icon(type="logout")
-          //-     span Logout
-          //- a-menu-item(key="4")
-          //-   a-icon(type="smile")
-          //-   span Aramıza Katıl!
-            //- a-menu-item(key="2")
-            //-     a-icon(type="enter")
-            //-     span Login
-            //- a-menu-item(key="3")
-            //-     a-icon(type="leave")
-            //-     span Logout
+ 
     a-layout
       a-layout-header(style="background:#fff; padding:0")
           a-icon(class="trigger" :type="collapsed ? 'menu-unfold' : 'menu-fold'" @click="collapsed = !collapsed")
       a-layout-content( :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }")
         router-view
 </template>
-
-<script>
-export default {
-  data: () => ({
-    collapsed: false,
-  }),
-};
-</script>
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
